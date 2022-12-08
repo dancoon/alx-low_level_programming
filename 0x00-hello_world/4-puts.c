2f56dc399f9144778ce13f5716ba6e1f1cabf248
dancoon <dancun.ian@students.jkuat.ac.ke>
2022-12-08 09:10:09 -0800

    Puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @puts() - used to write a line or string to the output(stdout) stream
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
