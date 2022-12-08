8c83d9471db88bedf451fa2d73f85f3d8336258c
dancoon <dancun.ian@students.jkuat.ac.ke>
2022-12-08 09:15:30 -0800

    puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * main - Entry point
+ * description: outputs Programming is like building a multilingual puzzle
  * @puts() - used to write a line or string to the output(stdout) stream
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
