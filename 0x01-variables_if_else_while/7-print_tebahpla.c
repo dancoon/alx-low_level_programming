3dd1ee9f8b053568638f52280fd255ccb0184312
dancoon <dancun.ian@students.jkuat.ac.ke>
2022-12-09 09:18:05 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descriptor: a program that prints the lowercase alphabet in
+ * reverse, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
