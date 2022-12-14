b79da7042e0b4751857ae35d8a3ef3f7e973880b
dancoon <dancun.ian@students.jkuat.ac.ke>
2022-12-14 08:41:06 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:A program that prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
