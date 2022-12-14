78aaf8303dfd54910859a9fb69d7156e3b56065c
dancoon <dancun.ian@students.jkuat.ac.ke>
2022-12-14 09:50:38 -0800

    Done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef main.h
+#define main.h
+
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -23,3 +26,5 @@ int add(int a, int b);
 void print_to_98(int n);
 
 void print_times_table(int n);
+
+#endif
--- 0x02-functions_nested_loops/main.h~
+++
@@ -1,2 +0,0 @@
-void print_alphabet(void);
-void print_alphabet_x10(void);
