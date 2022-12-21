b459e19b5ba532d3450fc0f0991cfe99aca0c76a
dancoon <dancun.ian@students.jkuat.ac.ke>
2022-12-21 11:00:10 -0800

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,24 +7,26 @@
  */
 char *cap_string(char *s)
 {
-int i = 0, j;
-char a[] = " \t\n,;.!?\"(){}";
-while (*(s + i))
-{
-if (*(s + i) >= 'a' && *(s + i) <= 'z')
-{
-if (i == 0)
-*(s + i) -= 'a' - 'A';
-else
-{
-for (j = 0; j <= 12; j++)
-{
-if (a[j] == *(s + i - 1))
-*(s + i) -= 'a' - 'A';
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
+	
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+				if (a[j] == *(s + i - 1))
+					*(s + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+		return (s);
 }
-}
-}
-i++;
-}
-return (s);
-}
+
