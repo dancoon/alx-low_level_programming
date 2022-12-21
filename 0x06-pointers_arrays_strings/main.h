80c58488d2f0eb10ae86eb58cd185d707fe49e6d
dancoon <dancun.ian@students.jkuat.ac.ke>
2022-12-21 11:33:44 -0800

    print numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,5 @@ char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_line(char *c, int s, int l);
-
 void print_buffer(char *b, int size);
-
+int _putchar(char c);
