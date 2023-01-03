861f3d0e31fa8a1878879a4c23834922fe245d55
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-03 08:51:31 -0800

    ALX

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a character
+ * @s: first parameter
+ * @to: second parameter
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
