d9bb5d0cc7e815787cd5de5bf9b85159a4487629
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-04 22:58:47 -0800

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,14 +3,14 @@
 /**
  * factorial - returns a factorial of a given number
  * @n: number
- * Returns: factorial
+ * Return: factorial
  */
 int factorial(int n)
 {
-	if ( n < 0)
+	if (n < 0)
 		return (-1);
 	if (n == 0)
-		return 1;
-	return (n * factorial(n -1));
+		return (1);
+	return (n * factorial(n - 1));
 }
 
