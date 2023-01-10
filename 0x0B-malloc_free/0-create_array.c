dfab81586b3eed4973f30c417ea4b6508155611c
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-09 21:54:25 -0800

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars and initialize
+ * @size: size of array
+ * @c: character
+ * Return: pointer or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+
+	char *ptr = (char *) malloc(sizeof(char) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		*(ptr + i) = c;
+	return (ptr);
+}
+
