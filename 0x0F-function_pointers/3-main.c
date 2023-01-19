a3543a1c2c402640f5e46f36cb3f7ae5bb7a257b
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-19 16:20:26 +0300

    alx

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,19 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 /**
- * main - entry point
- * @argc: arg count
- * @argv: arg vector
- * Return: Always 0 success
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to arguments
+ *
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	int i = argc;
-	char *s = argv[2];
-	int (*fun)(int, int) = get_op_func(s);
+	int a, b, c;
+	int (*f)(int, int);
 
-	int d = fun(1, 1);
-
-	printf("argc: %d %s %d\n", i, s, d);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = f(a, b);
+	printf("%d\n", c);
 	return (0);
 }
-
