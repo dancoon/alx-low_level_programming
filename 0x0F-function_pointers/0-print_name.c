88116e34122e05ad84ef8e30dcdb72ab92acb24b
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-18 23:52:09 -0800

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: first parameter
+ * @f: second parameter
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+}
+
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+void print_name(char *name, void (*f)(char *));
+#endif
+
