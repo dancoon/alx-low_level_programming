7c04e8e411b4961d488427e75cbd81f170b06fa7
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-19 01:50:23 -0800

    alx

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,19 +9,20 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i, t;
+	int i, t;
 
-    if (array != NULL && cmp != NULL)
-    {   
-    if (size > 0)
-    {
-        for (i = 0; i < size; i++)
-        {
-            t = cmp(array[i]);
-            if (t)
-                return (i);
-        }
-    }
-    return (-1);
-    }
+	if (array != NULL && cmp != NULL)
+	{
+		if (size > 0)
+		{
+			for (i = 0; i < size; i++)
+			{
+				t = cmp(array[i]);
+				if (t)
+					return (i);
+			}
+		}
+	}
+	return (-1);
 }
+
