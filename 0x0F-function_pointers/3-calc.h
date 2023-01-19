56cff77a4d2aaee790aacc5815c2f447e06a80bf
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-19 10:57:45 +0300

    alx

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,9 @@
+#ifndef _3_CALC_H_
+#define _3_CALC_H_
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+#endif
