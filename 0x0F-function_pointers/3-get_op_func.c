4ea9248ee32eff6465c6d52be63566a193d85f1c
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-19 16:17:42 +0300

    alx

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,28 +1,27 @@
 #include "3-calc.h"
 /**
  * get_op_func - returns a pointer to func
- * @s: first parameter
- * Return: pointer or NULL
- */
+* @s: first parameter
+* Return: pointer or NULL
+*/
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
 
-	i = 0;
-	while (i < 6)
-	{
-		if (strcmp(ops[i].op, s))
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
+i = 0;
+while (i < 6)
+{
+if (strcmp(ops[i].op, s))
+return (ops[i].f);
+i++;
 }
-
+return (NULL);
+}
