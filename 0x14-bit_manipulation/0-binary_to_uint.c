50cebb328a5fca2cb1ed041b34f5fe539217dc1c
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-02-06 17:50:38 +0300

    alx

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,90 +1,37 @@
 #include "main.h"
 
 /**
- * _atoi - converts chars to integer
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * @c: char to convert
- *
- * Return: converted integer
-*/
-unsigned int _atoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-
-/**
- * _strlen - gets the length of a string
- *
- * @str: string input
- *
- * Return: string length
-*/
-unsigned int _strlen(const char *str)
-{
-	unsigned int index = 0;
-
-	while (str[index] != '\0')
-		index++;
-	return (index);
-}
-
-/**
- * binary_to_uint - a function that converts a binary number
- *                  to an unsigned int
- *
- * @b: string that contains 0 and 1 characters
- *
- * Return: the number converted or 0
- *         if @b contains a character
- *         that is not 0 or 1 or when
- *         @b is null
-*/
+ * Return: unsigned int.
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	int index;
-	unsigned int result = 0, base2 = 1,  num = 0;
+	unsigned int ui;
+	int len, base_two;
 
-	/*if b is NULL return 0*/
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	#ifdef DEBUG /*print debug statement*/
-	printf("String is %s, and length is %u.\n", b, _strlen(b));
-	#endif
+	ui = 0;
 
-	/*iterate through string*/
-	for (index = _strlen(b) - 1; index >= 0; index--)
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		num = _atoi(b[index]); /*convert char to number*/
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2);
-		#endif
-
-		/*if number is not 0 or 1 return 0*/
-		if (num != 0 && num != 1)
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
+		}
 
-		result += num * base2; /*enable debug to see it in action*/
-		base2 *= 2;
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Result is %u.\n", result);
-		#endif
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-<<<<<<< HEAD
 
-	return (result);
-
+	return (ui);
 }
-=======
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		num <<= 1;
-		if (b[i] == '1')
-			num += 1;
-	}
-	return (num);
-}
-
->>>>>>> 4db68cc962e93fb062c65d6bcfe4d1e72533a40d
