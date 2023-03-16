cb64762f081601a01050c17e6fc8036ace0c768b
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-03-16 07:38:23 -0700

    Get node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (index == 0)
 		return (head);
 
-	while (j < index)
+	while (j <= index)
 	{
 		if (head->next == NULL)
 			return (NULL);
